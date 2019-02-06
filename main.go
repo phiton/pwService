488d15b0965771f3999f90aa1e0fbaca23b63881
Phi <phiton91@gmail.com>
2019-02-06 00:46:47 -0800

    Refactored and Added error handling

--- main.go
+++ main.go
@@ -6,8 +6,9 @@ import
     "log"
     "net/http"
     "encoding/json"
-    // "encoding/csv"
-    "github.com/gorilla/mux"
+    "encoding/csv"
+    "github.com/gorilla/mux"  //for extensibility purposes
+    "os"
 )
 
 type UserInfo struct {
@@ -21,13 +22,68 @@ type UserInfo struct {
 
 type UserInfos [] UserInfo
 
-func allUserInfos(w http.ResponseWriter, r *http.Request) {
-    UserInfos :=UserInfos {
-        UserInfo {Name:"Test Title", Uid: "Test Description", Gid: "Hello World", Comment: "hah", Home: "home", Shell: "shell"},
+func printJSON(w http.ResponseWriter, allEntries UserInfos ) {
+    //json.NewEncoder(w).Encode(allEntries)   //use this for non-pretty print
+    jsonEntry := json.NewEncoder(w)
+    jsonEntry.SetIndent("", "    ")
+    jsonEntry.Encode(allEntries)
+}
+
+func marshalPasswdFromFile(w http.ResponseWriter, filePath string) (allEntries UserInfos){
+    csvFile, err := os.Open (filePath)
+    if err != nil {
+        fmt.Println(err)
     }
 
-    fmt.Println("Endpoint Hit: All UserInfos Endpoint")
-    json.NewEncoder(w).Encode(UserInfos)
+    defer csvFile.Close()
+
+    reader := csv.NewReader(csvFile)
+    reader.Comma = ':'
+    reader.FieldsPerRecord = -1
+
+    csvData, err := reader.ReadAll()
+    if err != nil {
+        errorMsg := filePath + " may not have read access rights or does not exist" +
+                    " on this system"
+        fmt.Fprintf(w, errorMsg)
+        fmt.Println(err)
+    }
+
+    var oneEntry UserInfo
+    //var allEntries UserInfos
+    var lineNumber = 0
+    for _, each := range csvData {
+
+        lineNumber++
+        if each[0][0] == '#' {
+            continue
+        }
+
+        if len(each) != 7 {
+            fmt.Fprintf(w, "Error! passwd file may be corrupt!" +
+                " Found entry with %d fields on line:%d.", len(each), lineNumber)
+            fmt.Println("Error!:", filePath,  "file may be corrupt")
+            allEntries = nil
+            break
+        }
+        oneEntry.Name = each[0]
+        oneEntry.Uid = each[2]
+        oneEntry.Gid = each[3]
+        oneEntry.Comment = each[4]
+        oneEntry.Home = each[5]
+        oneEntry.Shell = each[6]
+        allEntries = append(allEntries, oneEntry)
+    }
+    return allEntries
+}
+
+func allUserInfos(w http.ResponseWriter, r *http.Request) {
+
+    fmt.Println("GET Endpoint Hit: /users")
+    allEntries := marshalPasswdFromFile(w, "/etc/passwd_test")
+    if (allEntries != nil) {
+        printJSON(w, allEntries)
+    }
 }
 
 func homePage(w http.ResponseWriter, r *http.Request) {
@@ -40,7 +96,8 @@ func handleRequests() {
     myRouter  := mux.NewRouter().StrictSlash(true)
 
     myRouter.HandleFunc("/", homePage)
-    myRouter.HandleFunc("/UserInfos", allUserInfos).Methods("GET")
+    myRouter.HandleFunc("/users", allUserInfos).Methods("GET")
+
     log.Fatal(http.ListenAndServe(":8081", myRouter))
 }
 
